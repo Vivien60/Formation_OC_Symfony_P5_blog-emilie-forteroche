07d138f4b8bed13ef83d020f6d0f0e57372441d4
Vivien60 <phedras.ch@gmail.com>
2025-08-20 00:39:00 +0200

    Refactoring css structure with pages and modules created rules partitioned into files under this struct

--- views/templates/admin.php
+++ views/templates/admin.php
@@ -5,14 +5,14 @@
      */
 ?>
 
-<div class="adminArticles">
-    <div class="adminArticles--header">
-        <h2>Edition des articles</h2>
-        <h2><a href="index.php?action=monitorArticles">Monitoring des articles</a></h2>
+<div class="adminArticles adminContent">
+    <div class="adminContent__header">
+        <h2 class="adminContent__header--aside">Edition des articles</h2>
+        <h2 class="adminContent__header--aside"><a href="index.php?action=monitorArticles">Monitoring des articles</a></h2>
     </div>
-    <div class="adminArticles__list">
+    <div class="adminArticles__list commentPaleTable">
         <?php foreach ($articles as $article) { ?>
-            <div class="articleLine">
+            <div class="articleLine tableLine">
                 <div class="title"><?= $article->getTitle() ?></div>
                 <div class="content"><?= $article->getContent(200) ?></div>
                 <div><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
--- views/templates/monitorArticles.php
+++ views/templates/monitorArticles.php
@@ -4,16 +4,17 @@
      * nombre de vues, nombre de commentaires, date de publication.
      */
 ?>
-<div class="monitoringArticles">
-    <div class="monitoringArticles--header">
-        <h2>Monitoring des articles</h2>
-        <h2><a href="index.php?action=admin">Edition des articles</a></h2>
+<div class="monitoringArticles adminContent">
+    <div class="adminContent__header">
+        <h2 class="adminContent__header--aside">Monitoring des articles</h2>
+        <h2 class="adminContent__header--aside"><a href="index.php?action=admin">Edition des articles</a></h2>
     </div>
-    <div class="monitoring__list">
-        <div class="monitoring__list--header title"><h3>Titre</h3><div class="monitoring__list--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
-        <div class="monitoring__list--header content"><h3>Vues</h3><div class="monitoring__list--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
-        <div class="monitoring__list--header content"><h3>Commentaires</h3><div class="monitoring__list--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
-        <div class="monitoring__list--header content"><h3>Date de publication</h3><div class="monitoring__list--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
+    <div class="monitoring__list commentPaleTable gridList">
+        <div class="gridList--header title"><h3>Titre</h3><div class="gridList--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
+        <div class="gridList--header content"><h3>Vues</h3><div class="gridList--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
+        <div class="gridList--header content"><h3>Commentaires</h3><div class="gridList--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
+        <div class="gridList--header content"><h3>Date de publication</h3><div class="gridList--order"><a href="?action=monitorArticles&order=title&dir=1">&#9207;</a>&nbsp;<a href="?action=monitorArticles&order=title&dir=0">&#9206;</a></div></div>
+        <div>&nbsp;</div>
         <?php foreach ($articles as $article) {
             /** @var Article $article */
             ?>
@@ -21,6 +22,7 @@
                 <div class="content"><?= $article->getNbViews() ?></div>
                 <div class="content"><?= $article->getNbComments() ?></div>
                 <div class="content"><?= $article->getDateCreation()->format('Y-m-d') ?></div>
+                <div><a class="submit gridList__cell--button" href="index.php?action=monitorComments&id=<?= $article->getId() ?>">Commentaires</a></div>
         <?php } ?>
     </div>
 </div>
